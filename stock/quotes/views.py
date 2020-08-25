a33d0c3d74812722989589de52291d4d582f7019
Jorge Silva <jsilvajr1@yahoo.com>
2020-08-24 22:46:55 -0500

    able to search for different companies

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -4,15 +4,19 @@ def home(request):
     import requests
     import json
 
-    # pk_3321d966bf1d440bb0289ace9cab26f1
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3321d966bf1d440bb0289ace9cab26f1")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_3321d966bf1d440bb0289ace9cab26f1")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "ERROR..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "ERROR..."
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol"})
 
-    return render(request, 'home.html', {'api': api})
+    
 
 def about(request):
     return render(request, 'about.html', {})
