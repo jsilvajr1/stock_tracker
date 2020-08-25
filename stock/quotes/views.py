4a351c1077af4f78e6c1be7ac7964b7f70d75c6d
Jorge Silva <jsilvajr1@yahoo.com>
2020-08-24 21:26:43 -0500

    added third party api and able to parce it

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # pk_3321d966bf1d440bb0289ace9cab26f1
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_3321d966bf1d440bb0289ace9cab26f1")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "ERROR..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
