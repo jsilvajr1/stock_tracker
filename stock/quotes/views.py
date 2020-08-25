ea625539f3737fad49a00df7d86f772e45715c55
Jorge Silva <jsilvajr1@yahoo.com>
2020-08-25 15:21:26 -0500

    added delete function to individual stocks

--- stock/quotes/views.py
+++ stock/quotes/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
+
+
 
 def home(request):
     import requests
@@ -23,4 +28,20 @@ def about(request):
 
 
 def add_stock(request):
-     return render(request, 'add_stock.html', {})
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock has been added"))
+            return redirect('add_stock')
+    
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted"))
+    return redirect(add_stock)
